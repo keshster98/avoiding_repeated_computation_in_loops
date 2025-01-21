29b15b48b3018ea3c9d18962d0b309cda5f1ab8f
Keshen <njkeshen98@gmail.com>
2025-01-21 09:27:29 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [numbers, setNumbers] = useState([5, 10, 15, 20, 25]);
+
+  const processNumbers = (arr) => {
+    let processed = [];
+    let n = arr.length;
+    // Optimized code start
+    for (let i = 0; i < n * n; i++) {
+      let j = Math.floor(i / n);
+      let k = i % n;
+
+      processed.push(numbers[j] * numbers[k]);
+    }
+    // Optimized code end
+
+    return processed;
+  };
+
+  const results = processNumbers(numbers);
+
+  const addNumber = () => {
+    const newNumbers = [...numbers];
+    newNumbers.push((newNumbers.length + 1) * 5);
+    setNumbers(newNumbers);
+  };
+
+  let renderedResults = [];
+  for (let i = 0; i < results.length; i++) {
+    renderedResults.push(<p key={i}>{results[i]}</p>);
+  }
+
+  return (
+    <div>
+      <h1>Avoid Repeated Computation Inside Loop</h1>
+      <button onClick={addNumber}>Add Number</button>
+      {renderedResults}
+    </div>
+  );
+};
+
+export default App;
